d6031e50a4d6134fdc18188ec05aceeecd0210b3
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-04-04 21:29:58 +0700

    'v2'

--- src/Array.java
+++ src/Array.java
@@ -35,4 +35,41 @@ public class Array {
         }
         return arr;
     }
+
+    public static String[] inputArr1(int _length){
+        Scanner input2=new Scanner(System.in);
+        String [] arr2 = new String [_length];
+        for (int i = 0; i <_length ; i++) {
+            arr2[i]=input2.nextLine();
+        }
+
+        return arr2;
+    }
+
+    public static int getValue(String [] arr){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Nhập chuỗi cần tìm kiếm");
+        String value=input.nextLine();
+        int i=0;
+        for(String element:arr){
+            if(element==value) {
+                System.out.println("Có tìm thấy chuỗi trong mảng "+ i);
+                return i;
+            }
+            i++;
+        }
+        return i;
+    }
+    public static int getMax(int [] arr){
+        int Max= arr[0];
+        for (int i = 1; i <arr.length ; i++) {
+            if(arr[i]>Max){
+                Max= arr[i];
+            }
+        }
+        System.out.println("Giá trị lớn nhất trong mảng là " + Max);
+        return Max;
+    }
+
+
 }
